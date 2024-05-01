267b3c3cb02da1f2c594ce33d64c8f92f2b5578b
pricaraujo <60271605+pricaraujo@users.noreply.github.com>
2024-05-01 13:13:03 -0300

    Update ModuleView.js

--- src/Screens/ModuleView.js
+++ src/Screens/ModuleView.js
@@ -17,7 +17,7 @@ function ModuleView() {
   axios.defaults.headers.post["Content-Type"] = "application/json";
   axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
   axios
-    .post("https://expert-gas-884de807bc21.herokuapp.com/measure", {
+    .get("https://expert-gas-884de807bc21.herokuapp.com/measure/latest/1", {
       cid: "teste-teste",
       client: "BAHIAGAS",
       measuredAt: "2021-12-10T13:49:51",
@@ -228,17 +228,6 @@ function ModuleView() {
         <div className="d-flex justify-content-center align-items-center">
           <Checkl />
           <img src={ModuleImage} alt="Modulo 1" />
-          <Link to="/view/otimization">
-            <Button
-              style={{
-                fontFamily: "Roboto",
-                marginRight: "75px",
-                marginBottom: "300px",
-              }}
-            >
-              Ver otimização
-            </Button>
-          </Link>
           { buildCheckList && <Checklist result={resultValue} />}
         </div>
       </div>
