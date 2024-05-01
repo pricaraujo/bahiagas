51e26551136f45e70834e789a4e1c1cfd08f60e7
pricaraujo <60271605+pricaraujo@users.noreply.github.com>
2024-05-01 15:06:00 -0300

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -23,18 +23,18 @@ class Navbar1 extends React.Component {
             <Navbar.Brand href="/">
               <img
                 src={logo}
-                width="150" // Ajuste o tamanho da logo aqui
-                height="50" // Ajuste o tamanho da logo aqui
+                width="150"
+                height="50"
                 className="d-inline-block align-top"
                 alt="Logo"
               />
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ml-auto"> {/* Trocamos 'mr-auto' por 'ml-auto' para alinhar à direita */}
+              <Nav className="ml-auto">
                 <Nav.Link href="/">HOME</Nav.Link>
                 <Nav.Link href="/about">SOBRE</Nav.Link>
-                <div style={{ marginLeft: '600px' }}> <Login /></div> {/* Reduzimos o padding */}
+                <div style={{ marginLeft: '600px' }}> <Login /></div>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
